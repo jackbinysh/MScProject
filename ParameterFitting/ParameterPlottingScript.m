edb161c82b43321ba4ce44888825d7ae537b0eca
jackbinysh <jack.binysh@gmail.com>
2015-07-29 19:32:55 +0100

    changing to matlab dynamics

--- ParameterFitting/ParameterPlottingScript.m
+++ ParameterFitting/ParameterPlottingScript.m
@@ -11,13 +11,15 @@ theta = out.solutions.recentbest.x .* varargin{1}.Scale;
 x0 = InitialState(theta); 
 
 %MEX
-cd('./MexRibodynamics')
+cd('./MatlabRibodynamics')
 ModelWithParams = @(t,x) RibodynamicsModel(t,x', theta);
 % get the model prediction
-[T,Prediction] = ode23s(ModelWithParams,Times,x0);
+[T,Prediction] = ode15s(ModelWithParams,Times,x0);
 cd('..')
 
 %plot the experimental data and simulation
 hold off
 plot(Times,Data); hold on;
 plot(Times,Prediction(:,6),'linewidth', 3.5);
+
+% the residuals
