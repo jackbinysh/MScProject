2b4dc1cbeb4ddb47af72f38cb88c8a77abdde19e
jackbinysh <jack.binysh@gmail.com>
2015-08-04 21:35:35 +0100

    fixing this syntax to be neater

--- ParameterFitting/Error.m
+++ ParameterFitting/Error.m
@@ -3,15 +3,17 @@ function Error = Error(theta,Varargin)
 % scaling the parameter vector up
 Scale = Varargin.Scale;
 theta = theta .* Scale;
+Dataset = Varargin.Dataset;
 
 %get our initial state from the models fixed point, given the parameters
-x0 = InitialState(theta,0,0);
+% the fixed point is solved for with IPTG on.
+x0 = InitialState(theta,0,1);
 
 %reading in the experimental time series
  persistent Times Data Fluorescence;
  if (isempty(Times))
-    Times = csvread(strcat('./data/InitialExperimentalData/time',Varargin.Dataset,'.csv'));
-    Data = csvread(strcat('./data/InitialExperimentalData/data',Varargin.Dataset,'.csv'));
+    Times = csvread(strcat('./data/InitialExperimentalData/time',Dataset,'.csv'));
+    Data = csvread(strcat('./data/InitialExperimentalData/data',Dataset,'.csv'));
     
     %if we've specified a specific run we are interested in, thats our data
     if(~isempty(Varargin.RunNumber))
@@ -22,15 +24,12 @@ x0 = InitialState(theta,0,0);
     end
  end
 
-%MATLAB
-cd('./MatlabRibodynamics')
-ModelWithParams = @(t,x) RibodynamicsModel(t,x', theta,Varargin.Dataset);
 % Set the ODE solver options. Provide a jacobian, and ensure the solver doesn't
 % step over a period of oscillation by setting a maximum step size.
 % http://uk.mathworks.com/help/matlab/ref/odeset.html
 
 options = odeset('Jacobian',@jacobian,'MaxStep',30); 
-[T,Prediction] = ode15s(ModelWithParams,Times,x0,options);
+[T,Prediction] = ode15s(@RibodynamicsModel,Times,x0,options,theta,Dataset);
 cd('..')
 
 Error = sum((Fluorescence - Prediction(:,6)).^2);
