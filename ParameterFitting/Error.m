f6bb99ed0fb9c1c41618c6adeb0fbf0cea96108c
jackbinysh <jack.binysh@gmail.com>
2015-08-04 11:23:40 +0100

    adding options to the ODE solver, to take the jacobian, and limit the step size to ensure it doesn't step over an oscillation

--- ParameterFitting/Error.m
+++ ParameterFitting/Error.m
@@ -5,7 +5,7 @@ Scale = Varargin.Scale;
 theta = theta .* Scale;
 
 %get our initial state from the models fixed point, given the parameters
-x0 = InitialState(theta);
+x0 = InitialState(theta,0,0);
 
 %reading in the experimental time series
  persistent Times Data Fluorescence;
@@ -22,11 +22,15 @@ x0 = InitialState(theta);
     end
  end
 
-%MEX
+%MATLAB
 cd('./MatlabRibodynamics')
 ModelWithParams = @(t,x) RibodynamicsModel(t,x', theta,Varargin.Dataset);
-% get the model prediction
-[T,Prediction] = ode15s(ModelWithParams,Times,x0);
+% Set the ODE solver options. Provide a jacobian, and ensure the solver doesn't
+% step over a period of oscillation by setting a maximum step size.
+% http://uk.mathworks.com/help/matlab/ref/odeset.html
+
+options = odeset('Jacobian',@jacobian,'MaxStep',30); 
+[T,Prediction] = ode15s(ModelWithParams,Times,x0,options);
 cd('..')
 
 Error = sum((Fluorescence - Prediction(:,6)).^2);
