1e5415e0f62fcdc82736aab7d0ac5ade2b0f3820
jackbinysh <jack.binysh@gmail.com>
2015-08-04 11:24:14 +0100

    little changes

--- ParameterFitting/ParameterPlottingScript.m
+++ ParameterFitting/ParameterPlottingScript.m
@@ -1,33 +1,39 @@
 clc; clear variables;
 format long g;
 
-%% Some bits and pieces of code for plotting
+%Some bits and pieces of code for plotting
 %This file contains various scraps of code I've used for plotting ,reading
 %data in, etc.
 
 % a workflow for reading in and analysing a single dataset
 
-% the experimental data
-Times = csvread('./data/InitialExperimentalData/time16_8.csv');
-Data = csvread('./data/InitialExperimentalData/data16_8.csv');
 % the solution found by optimisation
 load('variablescmaes.mat');
+
+% the experimental data
+Dataset = '13_9';
+Times = csvread(strcat('./data/InitialExperimentalData/time',Dataset,'.csv'));
+Data = csvread(strcat('./data/InitialExperimentalData/data',Dataset,'.csv'));
 % remember we have scaled this by our initial guess vector. So we scale
 % back
 
 theta = bestever.x .* varargin{1}.Scale;
-x0 = InitialState(theta); 
+x0 = InitialState(theta,0,0); 
 
 %MATLAB
 cd('./MatlabRibodynamics')
-ModelWithParams = @(t,x) RibodynamicsModel(t,x', theta);
+ModelWithParams = @(t,x) RibodynamicsModel(t,x', theta, Dataset);
 % get the model prediction
-[T,Prediction] = ode15s(ModelWithParams,Times,x0);
+options = odeset('MaxStep',1);
+sol = ode15s(ModelWithParams,Times,x0,options);
+[T,Prediction ]= ode15s(ModelWithParams,Times,x0,options);
 cd('..')
 
 %plot the experimental data and simulation
+hold on;
+plot(Times,Data);
 plot(Times,Prediction(:,6),'linewidth', 3.5);
-plot(Times,Data); hold on;
+hold off
 
 % the residuals
 figure(2);
@@ -59,11 +65,11 @@ for i = 1:length(alltheta)
     [T,Prediction] = ode15s(ModelWithParams,Times,x0);
     cd('..')
     plot(Times,Prediction(:,6),'linewidth', 3.5);
-    plot(Times,Data
+    plot(Times,Data);
     hold on;
 end
 
-Data(f<9)
-
+% a little code snippet to plot the forcing
+forcing = arrayfun(@(x) atc_input(x,'13_9'),Times)
 
     
