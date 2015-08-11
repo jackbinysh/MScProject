ef06e3535ef505fc8962ae18525e7df7174b69a3
jackbinysh <jack.binysh@gmail.com>
2015-08-11 13:47:44 +0100

    since we are putting the models in seperate folders, need to cd down to them

--- ParameterFitting/Error.m
+++ ParameterFitting/Error.m
@@ -5,10 +5,6 @@ Scale = Varargin.Scale;
 theta = theta .* Scale;
 Dataset = Varargin.Dataset;
 
-%get our initial state from the models fixed point, given the parameters
-% the fixed point is solved for with IPTG on.
-x0 = InitialState(theta,0,1);
-
 %reading in the experimental time series
  persistent Times Data Fluorescence;
  if (isempty(Times))
@@ -24,12 +20,17 @@ x0 = InitialState(theta,0,1);
     end
  end
 
+%get our initial state from the models fixed point, given the parameters
+% the fixed point is solved for with IPTG on.
+cd('./model2')
+x0 = InitialState(theta,0,1);
+
 % Set the ODE solver options. Provide a jacobian, and ensure the solver doesn't
 % step over a period of oscillation by setting a maximum step size.
 % http://uk.mathworks.com/help/matlab/ref/odeset.html
-
 options = odeset('Jacobian',@Jacobian,'MaxStep',30); 
 [T,Prediction] = ode15s(@RibodynamicsModel,Times,x0,options,theta,Dataset);
+cd('..')
 
-Error = sum((Fluorescence - Prediction(:,6)).^2);
+Error = sum((Fluorescence - Prediction(:,7)).^2);
 end
