b93d3c8f9783f9920fa3e003619cd11dc85eaca0
jackbinysh <jack.binysh@gmail.com>
2015-08-11 13:49:48 +0100

    adding the new variable into the initial guess

--- ParameterFitting/ParameterFittingScript.m
+++ ParameterFitting/ParameterFittingScript.m
@@ -9,21 +9,10 @@ RunNumber = [];
 
 % Initial Guess and bounds. Thesse  are currently set to the best guess
 % from GArun_30_07_2015.
-% list of variables {'f_srna';'k_on';'k_off';'k_hyb';'delta_m';'delta_s';'mu';'beta';'c'};
-InitialthetaLB = [1;1000;100000;1;1;1;0.00001;0.0001;1];
-InitialthetaUB = [1e3;1e6;1e8;1000;1000;1000;1;10;10000];
-% from GArun_30_07_2015.
-Initialtheta = [ 51.6568541032315
-          333746.163658966
-          35923992.3356378
-          78.0092377634537
-          52.5614560200565
-          40.2395554589868
-        0.0179550155596016
-         0.184074335687385
-          401.122143648984 ];
-
-
+%  % order should be {'f_srna';'k_on';'k_off';'k_hyb';'k_matur';'delta_m';'delta_s';'mu';'beta';'c'};
+InitialthetaLB = [1;1000;100000;1;1;1;1;0.00001;0.0001;1];
+InitialthetaUB = [1e3;1e6;1e8;1000;100000;1000;1000;1;10;10000];
+Initialtheta = [500;5e5;5e7;500;500;500;500;0.02;1;450];
 
 % plug into the wrapper function for the fitter
 [xmin,fmin,counteval,stopflag,out] = ParameterFit(Initialtheta, InitialthetaLB, InitialthetaUB, Dataset,RunNumber);
