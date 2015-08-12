f5fb036f85eef207078219a084ab575c8e0ddb1f
jackbinysh <jack.binysh@gmail.com>
2015-08-12 13:14:52 +0100

    removing the individual run fitting, and putting in a savenumber we can specify

--- ParameterFitting/Error.m
+++ ParameterFitting/Error.m
@@ -10,19 +10,12 @@ Dataset = Varargin.Dataset;
  if (isempty(Times))
     Times = csvread(strcat('./data/CleanedData/time',Dataset,'.csv'));
     Data = csvread(strcat('./data/CleanedData/data',Dataset,'.csv'));
-    
-    %if we've specified a specific run we are interested in, thats our data
-    if(~isempty(Varargin.RunNumber))
-        Fluorescence = Data(:,Varargin.RunNumber);
-    else
-    % if its empty, take the mean of all the data
-        Fluorescence = mean(Data,2);
-    end
+    Fluorescence = mean(Data,2);
  end
 
 %get our initial state from the models fixed point, given the parameters
 % the fixed point is solved for with IPTG on.
-cd('./model2')
+cd('./model1')
 x0 = InitialState(theta,0,1);
 
 % Set the ODE solver options. Provide a jacobian, and ensure the solver doesn't
@@ -32,5 +25,5 @@ options = odeset('Jacobian',@Jacobian,'MaxStep',30);
 [T,Prediction] = ode15s(@RibodynamicsModel,Times,x0,options,theta,Dataset);
 cd('..')
 
-Error = sum((Fluorescence - Prediction(:,7)).^2);
+Error = sum((Fluorescence - Prediction(:,6)).^2);
 end
--- ParameterFitting/ParameterFit.m
+++ ParameterFitting/ParameterFit.m
@@ -1,4 +1,4 @@
-function [xmin,fmin,counteval,stopflag,out] = ParameterFit(Initialtheta,InitialthetaLB,InitialthetaUB,Dataset,RunNumber)
+function [xmin,fmin,counteval,stopflag,out] = ParameterFit(Initialtheta,InitialthetaLB,InitialthetaUB,Dataset,SaveNumber)
 
     % reading off the initial guess and the bounds
     opts.LBounds = InitialthetaLB;  
@@ -12,12 +12,13 @@ function [xmin,fmin,counteval,stopflag,out] = ParameterFit(Initialtheta,Initialt
     Initialtheta = Initialtheta./Scale;
     
     % constructing the struct of additional arguments to pass to the objective
-    Varargin = struct('Scale',Scale,'Dataset',Dataset,'RunNumber',RunNumber);
+    Varargin = struct('Scale',Scale,'Dataset',Dataset);
     
     % setting some display etc. options
     opts.DispModulo = 1;
-    opts.SaveFilename = strcat('variablescmaes',num2str(RunNumber),'.mat');
-    opts.LogFilenamePrefix = strcat('outcmaes',num2str(RunNumber));
+    opts.Restarts = 2;
+    opts.SaveFilename = strcat('variablescmaes',Dataset,num2str(SaveNumber),'.mat');
+    opts.LogFilenamePrefix = strcat('outcmaes',Dataset,num2str(SaveNumber));
     
     
     % passing everyting into the algorithm
