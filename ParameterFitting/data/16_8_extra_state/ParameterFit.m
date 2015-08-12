ec13450b8b6ee4676310f8c14e9e5328799f6274
jackbinysh <jack.binysh@gmail.com>
2015-08-12 19:40:35 +0100

    another dataset

---
+++ ParameterFitting/data/16_8_extra_state/ParameterFit.m
@@ -0,0 +1,31 @@
+function [xmin,fmin,counteval,stopflag,out] = ParameterFit(Initialtheta,InitialthetaLB,InitialthetaUB,Dataset,RunNumber,SaveNumber)
+
+    % reading off the initial guess and the bounds
+    opts.LBounds = InitialthetaLB;  
+    opts.UBounds = InitialthetaUB;
+    
+    % The cmaes wants all parameters to be on the same scale. I take our
+    % initial Guess as setting the scale for the problem
+    Scale =  Initialtheta;
+    opts.LBounds = opts.LBounds./Scale;
+    opts.UBounds = opts.UBounds./Scale;
+    Initialtheta = Initialtheta./Scale;
+    
+    % constructing the struct of additional arguments to pass to the objective
+    Varargin = struct('Scale',Scale,'Dataset',Dataset,'RunNumber',RunNumber);
+    
+    % setting some display etc. options
+    opts.DispModulo = 1;
+    opts.Restarts = 3;
+    opts.SaveFilename = strcat('variablescmaes',num2str(SaveNumber),'.mat');
+    opts.LogFilenamePrefix = strcat('outcmaes',num2str(SaveNumber));
+    
+    
+    % passing everyting into the algorithm
+    [xmin,fmin,counteval,stopflag,out] = cmaes('Error',Initialtheta,[],opts,Varargin);
+end
+
+
+
+
+
---
+++ ParameterFitting/data/16_8_extra_state/README.txt
@@ -0,0 +1,3 @@
+12/08
+
+GA runs with the extra, immature SRNA state, to see if it improves the fitting.
