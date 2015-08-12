c8de9f77fa8d1aac73a720ccbbe57c2fda4f4f50
jackbinysh <jack.binysh@gmail.com>
2015-08-12 13:26:25 +0100

    altering the script to make it more COW friendly, replacing the run number with the save number

--- ParameterFitting/ParameterFittingScript.m
+++ ParameterFitting/ParameterFittingScript.m
@@ -1,18 +1,20 @@
-clc; clear variables;
+clc;
 format long g;
 
 % decide which dataset we are using
-Dataset = '16_8';
+Dataset = '14_7';
 
-% decide which run we use: if we leave it empty, an average is taken
-RunNumber = [];
+% Give a save number, if we want to do multiple runs.
+% im often setting this outside of the script, so I'll only set this now if
+% its empty
+if(isempty(SaveNumber)) SaveNumber = []; end
 
 % Initial Guess and bounds. Thesse  are currently set to the best guess
 % from GArun_30_07_2015.
-%  % order should be {'f_srna';'k_on';'k_off';'k_hyb';'k_matur';'delta_m';'delta_s';'mu';'beta';'c'};
+%  % order should be {'f_srna';'k_on';'k_off';'k_hyb';'delta_m';'delta_s';'mu';'beta';'c'};
 InitialthetaLB = [1;1000;100000;1;1;1;0.00001;0.0001;1];
 InitialthetaUB = [1e3;1e6;1e8;1000;1000;1000;1;10;10000];
 Initialtheta = [500;5e5;5e7;500;500;500;0.02;1;450];
 
 % plug into the wrapper function for the fitter
-[xmin,fmin,counteval,stopflag,out] = ParameterFit(Initialtheta, InitialthetaLB, InitialthetaUB, Dataset,RunNumber);
+[xmin,fmin,counteval,stopflag,out] = ParameterFit(Initialtheta, InitialthetaLB, InitialthetaUB, Dataset,SaveNumber);
