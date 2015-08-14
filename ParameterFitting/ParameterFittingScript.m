dab12bb56939f3410c8b1a352ba8b1658cfaeef7
jackbinysh <jack.binysh@gmail.com>
2015-08-14 12:55:12 +0100

    changing the method by which we select an initial test point to do the fitting

--- ParameterFitting/ParameterFittingScript.m
+++ ParameterFitting/ParameterFittingScript.m
@@ -5,15 +5,23 @@ format long g;
 % Give a save number, if we want to do multiple runs.
 % im often setting this outside of the script, so I'll only set this now if
 % its empty
-if(~exist('SaveNumber','var')) SaveNumber = []; end
+if(~exist('SaveNumber','var')) SaveNumber = [1]; end
 if(~exist('Dataset','var')) Dataset = {'13_9','14_7'}; end;
 
-% Initial Guess and bounds. Thesse  are currently set to the best guess
-% from GArun_30_07_2015.
-%  % order should be {'f_srna';'k_on';'k_off';'k_hyb';'delta_m';'delta_s';'mu';'beta';'c'};
-InitialthetaLB = [1;1000;100000;1;1;1;0.00001;0.0001;1];
-InitialthetaUB = [1e3;1e6;1e8;1000;1000;1000;1;10;10000];
-Initialtheta = [500;5e5;5e7;500;500;500;0.02;1;450];
+% put a random seed in to ensure different cluster runs don't start at the
+% same point 
+c = clock; secs = c(6);
+seed = SaveNumber + secs + cputime % display it to check its always different
+rng(seed, 'twister');
+
+% Initial Guess and bounds. We ballpark these for now, based on
+% the histograms of values we have gotten from earlier analyses.
+% order should be {'f_srna';'k_on';'k_off';'k_hyb';'delta_m';'delta_s';'mu';'beta';'c'};
+InitialthetaLB = [0.1;100;1;1;1;1;0.001;0.0001;300];
+InitialthetaUB = [1e4;1e7;1e8;10000;10000;1000;0.05;10;1000];
+
+% for an initial theta, we uniformly select a point in this hypercube
+Initialtheta = InitialthetaLB + (InitialthetaUB-InitialthetaLB ).*rand(length(InitialthetaLB),1);
 
 % plug into the wrapper function for the fitter
 [xmin,fmin,counteval,stopflag,out] = ParameterFit(Initialtheta, InitialthetaLB, InitialthetaUB, Dataset,SaveNumber);
