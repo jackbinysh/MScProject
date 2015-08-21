6598019e2b40be45624545eedc6f280199cd40c8
jackbinysh <jack.binysh@gmail.com>
2015-08-21 11:29:00 +0100

    I want to display the initial theta

--- ParameterFitting/ParameterFittingScript.m
+++ ParameterFitting/ParameterFittingScript.m
@@ -6,7 +6,7 @@ format long g;
 % im often setting this outside of the script, so I'll only set this now if
 % its empty
 if(~exist('SaveNumber','var')) SaveNumber = [1]; end
-if(~exist('Dataset','var')) Dataset = {'13_9','14_7'}; end;
+if(~exist('Dataset','var')) Dataset = {'13_9'}; end;
 
 % put a random seed in to ensure different cluster runs don't start at the
 % same point 
@@ -21,7 +21,7 @@ InitialthetaLB = [0.1;100;1;1;1;1;0.001;0.0001;300];
 InitialthetaUB = [1e4;1e7;1e8;10000;10000;1000;0.05;10;1000];
 
 % for an initial theta, we uniformly select a point in this hypercube
-Initialtheta = InitialthetaLB + (InitialthetaUB-InitialthetaLB ).*rand(length(InitialthetaLB),1);
+Initialtheta = InitialthetaLB + (InitialthetaUB-InitialthetaLB ).*rand(length(InitialthetaLB),1) % i want it displayed
 
 % plug into the wrapper function for the fitter
 [xmin,fmin,counteval,stopflag,out] = ParameterFit(Initialtheta, InitialthetaLB, InitialthetaUB, Dataset,SaveNumber);
