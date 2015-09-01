81eb2ccd57667b11ca7eeac0e1cd74b9c67ed864
jackbinysh <jack.binysh@gmail.com>
2015-09-01 14:28:55 +0100

    keep these bounds

--- ParameterFitting/ParameterFittingScript.m
+++ ParameterFitting/ParameterFittingScript.m
@@ -17,8 +17,8 @@ rng(seed, 'twister');
 % Initial Guess and bounds. We ballpark these for now, based on
 % the histograms of values we have gotten from earlier analyses.
 % order should be {'f_srna';'k_on';'k_off';'k_hyb';'delta_m';'delta_s';'mu';'beta';'c'};
-InitialthetaLB = [0.1;0.1;0.1;0.1;1;0.1;0.001;0.0001;300]
-InitialthetaUB = [1e4;1e7;1e8;10000;10000;1000;5;100;2000]
+InitialthetaLB = [0.1;1;1;1;1;1;0.001;0.0001;300]
+InitialthetaUB = [1e5;1e7;1e8;10000;10000;1000;0.05;10;1000]
 
 % for an initial theta, we uniformly select a point in this hypercube
 %Initialtheta = InitialthetaLB + (InitialthetaUB-InitialthetaLB ).*rand(length(InitialthetaLB),1) % i want it displayed
