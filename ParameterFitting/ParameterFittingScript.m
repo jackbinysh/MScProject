b1a4bf533cc013a6c2ab045b56aad03abd195614
jackbinysh <jack.binysh@gmail.com>
2015-09-01 14:29:58 +0100

    still use a random spread

--- ParameterFitting/ParameterFittingScript.m
+++ ParameterFitting/ParameterFittingScript.m
@@ -21,9 +21,7 @@ InitialthetaLB = [0.1;1;1;1;1;1;0.001;0.0001;300]
 InitialthetaUB = [1e5;1e7;1e8;10000;10000;1000;0.05;10;1000]
 
 % for an initial theta, we uniformly select a point in this hypercube
-%Initialtheta = InitialthetaLB + (InitialthetaUB-InitialthetaLB ).*rand(length(InitialthetaLB),1) % i want it displayed
-
-Initialtheta = [1476.09315060305;91027.8669948397;68425985.7997264;1;451.715025075103;1;0.0500000000000000;10;507.453643463837]
+Initialtheta = InitialthetaLB + (InitialthetaUB-InitialthetaLB ).*rand(length(InitialthetaLB),1) % i want it displayed
 
 % plug into the wrapper function for the fitter
 [xmin,fmin,counteval,stopflag,out] = ParameterFit(Initialtheta, InitialthetaLB, InitialthetaUB, Dataset,SaveNumber);
