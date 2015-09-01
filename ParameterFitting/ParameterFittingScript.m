de4c179e86b69f07131dcc2652e6573e828e8fb4
jackbinysh <jack.binysh@gmail.com>
2015-09-01 11:51:01 +0100

    changing starting point to 13_9 best guess

--- ParameterFitting/ParameterFittingScript.m
+++ ParameterFitting/ParameterFittingScript.m
@@ -17,11 +17,13 @@ rng(seed, 'twister');
 % Initial Guess and bounds. We ballpark these for now, based on
 % the histograms of values we have gotten from earlier analyses.
 % order should be {'f_srna';'k_on';'k_off';'k_hyb';'delta_m';'delta_s';'mu';'beta';'c'};
-InitialthetaLB = [0.1;100;1;1;1;1;0.001;0.0001;300];
-InitialthetaUB = [1e4;1e7;1e8;10000;10000;1000;0.05;10;1000];
+InitialthetaLB = [0.1;100;1;0.1;1;0.1;0.001;0.0001;300];
+InitialthetaUB = [1e4;1e7;1e8;10000;10000;1000;5;100;1000];
 
 % for an initial theta, we uniformly select a point in this hypercube
-Initialtheta = InitialthetaLB + (InitialthetaUB-InitialthetaLB ).*rand(length(InitialthetaLB),1) % i want it displayed
+%Initialtheta = InitialthetaLB + (InitialthetaUB-InitialthetaLB ).*rand(length(InitialthetaLB),1) % i want it displayed
+
+Initialtheta = [1476.09315060305;91027.8669948397;68425985.7997264;1;451.715025075103;1;0.0500000000000000;10;507.453643463837];
 
 % plug into the wrapper function for the fitter
 [xmin,fmin,counteval,stopflag,out] = ParameterFit(Initialtheta, InitialthetaLB, InitialthetaUB, Dataset,SaveNumber);
